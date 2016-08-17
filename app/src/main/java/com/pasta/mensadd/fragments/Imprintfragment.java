8c32d2ead80022c71da51c837a8a4e7b11c18a99
julianctni <julian.catoni@tu-dresden.de>
2016-08-17 16:42:24 +0200

    Added some content to imprint

--- app/src/main/java/com/pasta/mensadd/fragments/Imprintfragment.java
+++ app/src/main/java/com/pasta/mensadd/fragments/Imprintfragment.java
@@ -3,28 +3,27 @@ package com.pasta.mensadd.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.pasta.mensadd.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Imprintfragment extends Fragment {
-
-
-    public Imprintfragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imprint, container, false);
+        View v = inflater.inflate(R.layout.fragment_imprint, container, false);
+        TextView licenseView = (TextView) v.findViewById(R.id.imprintLicense);
+
+        licenseView.setMovementMethod(LinkMovementMethod.getInstance());
+        licenseView.setText(Html
+                .fromHtml(getString(R.string.imprint_license)));
+        return v;
     }
 
 }
