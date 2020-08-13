e4f3439b7d8b8099747dec8017dccd2b79e4a648
Julian <julianctni@mailbox.org>
2020-08-13 20:14:25 +0200

    Refactor ImprintFragment

--- app/src/main/java/com/pasta/mensadd/ui/fragments/ImprintFragment.java
+++ app/src/main/java/com/pasta/mensadd/ui/fragments/ImprintFragment.java
@@ -24,59 +24,57 @@ import com.pasta.mensadd.PreferenceService;
 import com.pasta.mensadd.R;
 import com.pasta.mensadd.ui.MainActivity;
 
+import org.jetbrains.annotations.NotNull;
+
 public class ImprintFragment extends Fragment implements View.OnClickListener {
 
+    private final static String SUPPORT_MAIL_ADDRESS = "julianctni@gmail.com";
+    private final static String SUPPORT_MAIL_SUBJECT = "Feedback mensaDD";
+    private final static String URI_SCHEME_MAILTO = "mailto";
     private int mEasterCount = 0;
     private PreferenceService mPreferenceService;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_imprint, container, false);
+        View view = inflater.inflate(R.layout.fragment_imprint, container, false);
         setHasOptionsMenu(true);
-        TextView baconView = v.findViewById(R.id.imprintLicenseBacon);
+        TextView baconView = view.findViewById(R.id.imprintLicenseBacon);
         mPreferenceService = new PreferenceService(requireContext());
         if (mPreferenceService.isBaconFeatureEnabled())
             baconView.setVisibility(View.VISIBLE);
-        TextView licenseView = v.findViewById(R.id.imprintLicense);
+        TextView licenseView = view.findViewById(R.id.imprintLicense);
 
         licenseView.setMovementMethod(LinkMovementMethod.getInstance());
         licenseView.setText(Html
                 .fromHtml(getString(R.string.imprint_license)));
-        ImageView banner = v.findViewById(R.id.banner_imprint);
+        ImageView banner = view.findViewById(R.id.banner_imprint);
         banner.setOnClickListener(this);
         MainActivity activity = (MainActivity) getActivity();
         if (activity != null)
             activity.updateToolbar(-1, getString(R.string.pref_imprint));
-        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        Toolbar toolbar = requireActivity().findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().onBackPressed();
-            }
-        });
-        return v;
+        toolbar.setNavigationOnClickListener(v -> requireActivity().onBackPressed());
+        return view;
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+    public void onCreateOptionsMenu(@NotNull Menu menu, MenuInflater menuInflater) {
         menuInflater.inflate(R.menu.fragment_imprint_menu, menu);
         super.onCreateOptionsMenu(menu, menuInflater);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.feedback:
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                        "mailto", "julianctni@gmail.com", null));
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "feedback mensaDD");
-                startActivity(Intent.createChooser(emailIntent, getString(R.string.send_feedback_mail)));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.feedback) {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    URI_SCHEME_MAILTO, SUPPORT_MAIL_ADDRESS, null));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, SUPPORT_MAIL_SUBJECT);
+            startActivity(Intent.createChooser(emailIntent, getString(R.string.send_feedback_mail)));
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
